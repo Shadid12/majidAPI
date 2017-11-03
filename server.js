1bacc1582eeb558d828d4bb1b9356d64e2629abb
shadid12 <shadidhaque2012@gmail.com>
2017-11-03 13:41:41 -0400

    whatever was added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(keys.mongoURI);
 
 // allow CORS
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
