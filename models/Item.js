5a07c946ef448c6f84729c2eef7702d1c77a1eee
shadid12 <shadidhaque2012@gmail.com>
2017-11-10 19:09:51 -0500

    count endpoint added

--- models/Item.js
+++ models/Item.js
@@ -4,8 +4,6 @@ const { Schema } = mongoose;
 const ItemSchema = new Schema({
   username: String,
   address:  String,
-  postal:   String,
-  city:     String,
   pin:      String 
 });
 
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -9,8 +9,6 @@ module.exports = app => {
 		console.log(req.body.username);
 		item.username = req.body.username;
 		item.address  = req.body.address;
-		item.postal   = req.body.postal ;
-		item.city     = req.body.city;
 		item.pin      = req.body.pin;
 		item.save((err) => {
 			if(!err){
@@ -26,7 +24,7 @@ module.exports = app => {
 		})
 	})
 
-	// get all the lists of the items
+	// get item by username
 	app.get('/index', (req, res) => {
 		    Item.aggregate([
 		        {$group : {_id : "$username", 
@@ -43,6 +41,16 @@ module.exports = app => {
 		        res.json(result);
 		    });
 	});
+
+	// get count by username
+	app.get('/count/:username', (req, res) => {
+		Item.find({ username: req.params.username }, (err, result) => {
+			if(!err) {
+				res.json(result.length);
+			}
+		})
+	})
+
 	// delete
 	app.delete('/', (req, res) => {
 		Item.remove({}, (err, result) => {
